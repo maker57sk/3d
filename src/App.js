9964d32473548321416edaf5740f029fb88f2b8f
maker57 <maker57sk@gmail.com>
2021-03-11 14:56:11 +0400

    basic 3d with threejs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+// import { Physics} from 'use-cannon';
 import './App.css';
 
-function App() {
+function Box() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh position={[0, 2, 0]}>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  )
 }
 
-export default App;
+function Plane() {
+  return (
+    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  )
+}
+
+
+export default function App() {
+  return (
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <spotLight
+        position={[10, 15, 10]} angle={0.5}
+      />
+     
+        <Box />
+        <Plane />
+     
+    </Canvas>
+  )
+}
+
+
